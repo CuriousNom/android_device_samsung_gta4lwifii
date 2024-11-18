4d0106af6c4c2c32144d5467f61b958ace4cf932
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2024-11-18 17:47:04 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
-git clone --depth=1 https://github.com/Matrixx-Devices/android_device_samsung_gta4l-common device/samsung/gta4l-common
-git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4l-common vendor/samsung/gta4l-common
-git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4lwifi vendor/samsung/gta4lwifi
-git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l kernel/samsung/gta4l
-git clone --depth=1 https://github.com/Matrixx-Devices/android_hardware_samsung hardware/tmp
-cp -r hardware/tmp/* hardware/samsung/
-rm -rf hardware/tmp
+#git clone --depth=1 https://github.com/Matrixx-Devices/android_device_samsung_gta4l-common device/samsung/gta4l-common
+#git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4l-common vendor/samsung/gta4l-common
+#git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4lwifi vendor/samsung/gta4lwifi
+#git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l kernel/samsung/gta4l
+#git clone --depth=1 https://github.com/Matrixx-Devices/android_hardware_samsung hardware/tmp
+#cp -r hardware/tmp/* hardware/samsung/
+#rm -rf hardware/tmp
 
 mkdir prebuilts/clang/host/linux-x86/clang-neutron; cd prebuilts/clang/host/linux-x86/clang-neutron;
 curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman";
-chmod +x antman; ./antman -S=05012024; ./antman --patch=glibc; cd ../../../../../;sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/lineage/build/tasks/kernel.mk;
+chmod +x antman; ./antman -S; ./antman --patch=glibc; cd ../../../../../;sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/lineage/build/tasks/kernel.mk;
